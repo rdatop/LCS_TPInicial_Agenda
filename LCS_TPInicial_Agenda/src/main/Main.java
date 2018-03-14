d902874ed42c3c3cfd3aa578c7d7f0bfe8312a31
rdatop <rdatop@yahoo.com.ar>
2018-03-14 00:39:49 -0300

    prueba comentario en main import

--- LCS_TPInicial_Agenda/src/main/Main.java
+++ LCS_TPInicial_Agenda/src/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 
 import modelo.Agenda;
-import persistencia.conexion.Conexion;//consultal por este problema
+import persistencia.conexion.Conexion;//consultal por este problemita
 import persistencia.dao.mysql.DAOSQLFactory;
 import presentacion.controlador.Controlador;
 import presentacion.vista.Vista;
